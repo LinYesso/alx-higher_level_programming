372e1a7f2c794d7520794aa805de730368d69078
LinYesso <lindaangeaminatayesso@gmail.com>
2022-06-08 05:39:44 -0700

    correct the pycodestyle

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     copy = []
-    for l in my_list:
-        if l == search:
+    for list_ in my_list:
+        if list_ == search:
             copy.append(replace)
         else:
-             copy.append(l)
+            copy.append(list_)
     return copy
