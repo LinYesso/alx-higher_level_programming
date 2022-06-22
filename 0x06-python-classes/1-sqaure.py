61ccbb76b23cb905b0a6790ac2b85fc75efdda2a
LinYesso <lindaangeaminatayesso@gmail.com>
2022-06-21 21:39:41 -0700

    first task

---
+++ 0x06-python-classes/1-sqaure.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    """A class that represents a square.
+    Private instance attribute: size.
+    Instantiation with size (no type/value verification).
+    """
+
+    def __init__(self, size):
+        """Initializes the data size."""
+        self.__size = size
