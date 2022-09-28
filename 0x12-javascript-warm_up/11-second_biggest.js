688d587d5f0497b5444bb78a97af98ed3c32a258
Linda Yesso <lindaangeaminatayesso@gmail.com>
2022-09-28 14:29:20 +0000

    11-second_biggest.js file

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (process.argv.length <= 3) {
+  console.log('0');
+} else {
+  let  numbers = process.argv.slice(2).map(Number);
+  numbers = numbers.sort(function (a, b) { return b - a; });
+  console.log(numbers[1]);
+}
